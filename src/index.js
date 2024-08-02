0aaf345cd2a203c0b5855f85f6bf1f326ccb88b9
Danila <danila.tsatsuk@trackera.io>
2024-08-02 15:03:54 +0300

    feat: add reverse function to reverse a number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = n.toString();
+    let reversedStr = str.split("").reverse().join("");
+    let reversedNum = parseInt(reversedStr, 10);
+    return reversedNum;
+};
